4d0405eea502e848c4d5e2384fcc088d04812726
pezy4303675 <maracujapezy@gmail.com>
2025-04-13 23:59:20 +0100

    popopo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,6 +44,24 @@ function Home({ searchQuery }) {
     }
   }, [searchQuery, produtos]);
 
+
+
+// Inserir script exatamente no ponto indicado
+useEffect(() => {
+  if (!loading && filteredProducts.length > 0 && scriptContainerRef.current) {
+    const script = document.createElement('script');
+    script.src = "//racuchica.com/b/XxVRs.dXGdlS0xYUWzcJ/-ermF9fujZAUslRkUP/T/Y/y-NujDc/zDNVTFYdtbNpj/IO2FNbz/Mt1PNwwn";
+    script.async = true;
+    script.referrerPolicy = 'no-referrer-when-downgrade';
+    scriptContainerRef.current.appendChild(script);
+
+    return () => {
+      script.remove();
+    };
+  }
+}, [loading, filteredProducts]);
+
+
   if (loading) return <div>Carregando...</div>;
   if (error) return <div>{error}</div>;
 
